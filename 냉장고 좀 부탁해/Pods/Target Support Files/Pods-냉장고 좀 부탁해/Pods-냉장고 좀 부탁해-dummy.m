13364d66e21c8f1df4047117acfd13a7c3066672
이현욱 <hjhsha827@gmail.com>
2022-07-09 05:25:13 +0900

    [start] 프로젝트 시작
    이번엔 마무리 보자

---
+++ 냉장고 좀 부탁해/Pods/Target Support Files/Pods-냉장고 좀 부탁해/Pods-냉장고 좀 부탁해-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_____________________ : NSObject
+@end
+@implementation PodsDummy_Pods_____________________
+@end
---
+++ 냉장고 좀 부탁해/냉장고 좀 부탁해/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  냉장고 좀 부탁해
+//
+//  Created by 이현욱 on 2022/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
